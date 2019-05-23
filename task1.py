fbffa259f9acb0356348dabe27de0fefe0622c8b
abhhi33 <abhishek.agarwal@dal.ca>
2019-05-23 10:10:23 -0300

    task1

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
-def input():
+
+def input_option():
 	target_file=input("book1,book2,book3...:")
 	check=0	
 	if target_file="book1":
@@ -11,6 +12,13 @@ def input():
 	elif:
 		target_file=input("Please enrter the valid input")
 		checked
-	input_page(fr_
+	input_page(fr)
+
 input option()
-	
+
+def input_page(fr):
+	fw=open("Newfile.txt","w")
+	count=0 
+	pagestart= input("enter the starting page number")
+	pagend=input("enter the last page number")
+			
