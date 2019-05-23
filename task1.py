848a0c3b290517497bc61e4dafc75d61e8f68622
abhhi33 <abhishek.agarwal@dal.ca>
2019-05-23 10:02:39 -0300

    task1

--- task1.py
+++ task1.py
@@ -1,1 +1,16 @@
-def 
+def input():
+	target_file=input("book1,book2,book3...:")
+	check=0	
+	if target_file="book1":
+		fread= open("book1.txt","r")
+	elif target_file="book2":
+		fread=open ("book2.txt,"r")
+	elif target_file="book3":
+		fread= open("book3.txt","r")
+		check=1
+	elif:
+		target_file=input("Please enrter the valid input")
+		checked
+	input_page(fr_
+input option()
+	
