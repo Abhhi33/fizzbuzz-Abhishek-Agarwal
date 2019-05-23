2c4b1bda3cfcfcad9a3c7695d0bf6dd2dd54c542
abhhi33 <abhishek.agarwal@dal.ca>
2019-05-23 10:39:51 -0300

    task1

--- task1.py
+++ task1.py
@@ -21,4 +21,13 @@ def input_page(fr):
 	count=0 
 	pagestart= input("enter the starting page number")
 	pagend=input("enter the last page number")
-			
+	for word in range(0,(25*pages),+1):
+	count+=1
+	if count>=(25*pages) and count<=(25*pageend):
+		for j in i:
+			if j=='o' or j=='0':
+				j='0'
+			elif j=='e' or j=='E':
+				j='3'
+			elif j=='i' or j=='I'
+				j='1'
